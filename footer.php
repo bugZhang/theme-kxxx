8699900c40f6f4d5c7c6701b64f2e9fb77c8bca7
Jerry Zhang <zjk6700@gmail.com>
2017-08-18 22:45:10 +0800

    谷歌统计

--- footer.php
+++ footer.php
@@ -37,6 +37,13 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8912822468255347",
+    enable_page_level_ads: true
+  });
+</script>
 
 </body>
 </html>
